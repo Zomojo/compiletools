ea217d655bff3c406ac768b694f26831b64848e9
Brendan Babb <brendan.babb@zomojo.com>
2011-12-08 12:39:02 +1100

    remove control chars from README

--- create-documentation.sh
+++ create-documentation.sh
@@ -6,5 +6,5 @@
 # Do not edit cake.1 or README explicilty. Let this script create them.
 
 help2man --output=cake.1 --no-info --include=help2man.include ./cake
-nroff -c -mandoc cake.1 > README
+nroff -c -mandoc cake.1 | col -b > README
 
