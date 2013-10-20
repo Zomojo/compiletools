507ee8abecc6e0d09f3b1504a6165c15282c2aa1
DrGeoff <drgeoffathome@gmail.com>
2013-10-20 17:56:41 +1000

    Make test.sh use sh  rather than bash

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 set -e
 
@@ -9,7 +9,7 @@ set -e
 ./cake --quiet tests/helloworld_cpp.cpp
 
 result=$(bin/helloworld_cpp)
-if [[ $result != "debug 1" ]]; then
+if [ "$result" != "debug 1" ]; then
     echo test 1: Incorrect variant: $result
     exit 1
 fi
@@ -17,13 +17,13 @@ fi
 ./cake --quiet tests/helloworld_cpp.cpp --variant=release
 
 result=$(bin/helloworld_cpp)
-if [[ $result != "release 1" ]]; then
+if [ "$result" != "release 1" ]; then
     echo test 2: Incorrect variant: $result
     exit 1
 fi
 
 result=$(bin/helloworld_cpp extra args)
-if [[ $result != "release 3" ]]; then
+if [ "$result" != "release 3" ]; then
     echo test 3: Incorrect args: $result
     exit 1
 fi
@@ -36,7 +36,7 @@ fi
 ./cake --quiet tests/helloworld_c.c
 
 result=$(bin/helloworld_c)
-if [[ $result != "debug 1" ]]; then
+if [ "$result" != "debug 1" ]; then
     echo test 4: Incorrect variant: $result
     exit 1
 fi
@@ -44,13 +44,13 @@ fi
 ./cake --quiet tests/helloworld_c.c --variant=release
 
 result=$(bin/helloworld_c)
-if [[ $result != "release 1" ]]; then
+if [ "$result" != "release 1" ]; then
     echo test 5: Incorrect variant: $result
     exit 1
 fi
 
 result=$(bin/helloworld_c extra args)
-if [[ $result != "release 3" ]]; then
+if [ "$result" != "release 3" ]; then
     echo test 6: Incorrect args: $result
     exit 1
 fi
@@ -59,7 +59,7 @@ fi
 # Test that c compilation picks up //#CFLAGS
 #
 ./cake --quiet tests/test_cflags.c
-if [[ $? != 0 ]]; then
+if [ $? != 0 ]; then
     echo test 7: cake does not detect the //#CFLAGS in a c file
     exit 1
 fi
@@ -71,7 +71,7 @@ rm -rf bin/*
 ./cake --static-library tests/get_numbers.cpp
 ./cake tests/test_library.cpp
 result=$(bin/test_library)
-if [[ $result != "1 2" ]]; then
+if [ "$result" != "1 2" ]; then
     echo test 5: Incorrect result from static library test: $result
     exit 1
 fi
@@ -86,7 +86,7 @@ export LD_LIBRARY_PATH
 ./cake tests/test_library.cpp
 result=$(bin/test_library)
 unset LD_LIBRARY_PATH
-if [[ $result != "1 2" ]]; then
+if [ "$result" != "1 2" ]; then
     echo test 5: Incorrect result from dynamic library test: $result
     exit 1
 fi
