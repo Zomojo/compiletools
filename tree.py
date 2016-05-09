ee58aaa024cc7f0f92b8b051b713c486bd4aa14c
Geoff Ericksson <geoff.ericksson@zomojo.com>
2016-05-09 21:08:50 +1000

    headertree produces a usable tree

--- tree.py
+++ tree.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+import inspect
 
 
 def tree():
@@ -11,3 +12,25 @@ def dicts(tt):
         For example, to pretty print, pprint(dicts(tree_obj))
     """
     return {key: dicts(tt[key]) for key in tt}
+
+
+def traverse(tree, function, traverse_before_function_call=True, depth=0):
+    """ Traverse a tree, calling the given function on the nodes """
+    function_uses_depth = 'depth' in inspect.getargspec(function).args
+    for key, value in tree.items():
+        if traverse_before_function_call:
+            traverse(
+                tree=value,
+                function=function,
+                traverse_before_function_call=traverse_before_function_call,
+                depth=depth + 1)
+        if function_uses_depth:
+            function(key, depth=depth)
+        else:
+            function(key)
+        if not traverse_before_function_call:
+            traverse(
+                tree=value,
+                function=function,
+                traverse_before_function_call=traverse_before_function_call,
+                depth=depth + 1)
