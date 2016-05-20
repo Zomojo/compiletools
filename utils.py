2e61b263cbc8d3c8564b2a5e4b8eff5ac1623cf8
DrGeoff <drgeoffathome@gmail.com>
2016-05-20 21:41:08 +1000

    Minor help change

--- utils.py
+++ utils.py
@@ -27,7 +27,7 @@ def add_boolean_argument(parser, name, dest, default=False, help=None):
         default=default,
         const=True,
         type=to_bool,
-        help=help + "--no-" + name + " to turn the feature off.")
+        help=help + " Use --no-" + name + " to turn the feature off.")
     group.add_argument('--no-' + name, dest=dest, action='store_false')
 
 
