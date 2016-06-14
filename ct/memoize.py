e1cb3cda2c4e240b1d4b715a5ecf13daf9606a59
Geoff Ericksson <geoff.ericksson@zomojo.com>
2016-06-14 13:31:37 +1000

    Create a decorator that memoizes False results

--- ct/memoize.py
+++ ct/memoize.py
@@ -15,3 +15,19 @@ def memoize(obj):
         return cache[key]
     return memoizer
 
+def memoize_false(func):
+    """ For a function that can only return true or false, memoize the false results """
+    cache = set()
+
+    @functools.wraps(func)
+    def memoizer(*args, **kwargs):
+        key = str(args) + str(kwargs)
+        if key in cache:
+            return False
+        else:
+            result = func(*args, **kwargs)
+            if not result:
+                cache.add(key)
+            return result
+    return memoizer
+
