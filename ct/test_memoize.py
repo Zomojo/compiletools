9998e6ea36bf25bdd647975137e40cb1cc7d7013
DrGeoff <drgeoffathome@gmail.com>
2016-06-14 21:55:24 +1000

    Start of testing memoize

---
+++ ct/test_memoize.py
@@ -0,0 +1,25 @@
+from __future__ import print_function
+import unittest
+import time
+from ct.memoize import memoize
+
+@memoize
+def noargsfunc():
+    time.sleep(0.51)
+    return 42
+
+class TestMemoize(unittest.TestCase):
+
+    def test_noargsfunc(self):
+        start = time.time()
+        output = noargsfunc()
+        self.assertEqual(42, output)
+        self.assertTrue( time.time() - start > 0.5)
+
+        start = time.time()
+        output2 = noargsfunc()
+        self.assertEqual(42, output2)
+        self.assertTrue( time.time() - start < 0.1)
+
+if __name__ == '__main__':
+    unittest.main()
