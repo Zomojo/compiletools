4a755e5db854d6cd663bb57cdcbea85719979d52
Geoff Ericksson <geoff.ericksson@zomojo.com>
2016-07-22 10:46:30 +1000

    ct-cake can "prepend" as well as "append" options.  Link options now processed.

--- ct/cake.py
+++ ct/cake.py
@@ -17,6 +17,26 @@ class Cake:
     @staticmethod
     def add_arguments(cap, variant, argv):
         cap.add(
+            "--prepend-CPPFLAGS",
+            dest='prependcppflags',
+            help="Prepends the given text to the CPPFLAGS already set. Useful for adding search paths etc. ")
+        cap.add(
+            "--prepend-CFLAGS",
+            dest='prependcflags',
+            help="Prepends the given text to the CFLAGS already set. Useful for adding search paths etc. ")
+        cap.add(
+            "--prepend-CXXFLAGS",
+            dest='prependcxxflags',
+            help="Prepends the given text to the CXXFLAGS already set. Useful for adding search paths etc. ")
+        cap.add(
+            "--prepend-LDFLAGS",
+            dest='prependldflags',
+            help="Prepends the given text to the LDFLAGS already set. Useful for adding search paths etc. ")
+        cap.add(
+            "--prepend-LINKFLAGS",
+            dest='prependldflags',
+            help="Prepends the given text to the LDFLAGS already set. Synonym for prepend-LDFLAGS. Useful for adding search paths etc. ")
+        cap.add(
             "--append-CPPFLAGS",
             dest='appendcppflags',
             help="Appends the given text to the CPPFLAGS already set. Useful for adding search paths etc. ")
@@ -28,13 +48,21 @@ class Cake:
             "--append-CXXFLAGS",
             dest='appendcxxflags',
             help="Appends the given text to the CXXFLAGS already set. Useful for adding search paths etc. ")
+        cap.add(
+            "--append-LDFLAGS",
+            dest='appendldflags',
+            help="Prepends the given text to the LDFLAGS already set. Useful for adding search paths etc. ")
+        cap.add(
+            "--append-LINKFLAGS",
+            dest='appendldflags',
+            help="Prepends the given text to the LDFLAGS already set. Synonym for append-LDFLAGS. Useful for adding search paths etc. ")
         ct.utils.add_boolean_argument(
             parser=cap,
             name="file-list",
             dest='filelist',
                 default=False,
                 help="Print list of referenced files.")        
-        ct.makefile.MakefileCreator.add_arguments(cap, variant, argv)
+        ct.makefile.MakefileCreator.add_arguments(cap)
         cap.add(
             "--begintests",
             dest='tests',
@@ -68,12 +96,22 @@ class Cake:
         """ Transform the arguments into suitable versions for ct-* tools 
             and call the appropriate tool.
         """
+        if self.args.prependcppflags:
+            self.args.CPPFLAGS = " ".join([self.args.prependcppflags, self.args.CPPFLAGS])
+        if self.args.prependcflags:
+            self.args.CFLAGS = " ".join([self.args.prependcflags, self.args.CFLAGS])
+        if self.args.prependcxxflags:
+            self.args.CXXFLAGS = " ".join([self.args.prependcxxflags, self.args.CXXFLAGS])
+        if self.args.prependldflags:
+            self.args.LDFLAGS = " ".join([self.args.prependldflags, self.args.LDFLAGS])
         if self.args.appendcppflags:
             self.args.CPPFLAGS += " " + self.args.appendcppflags
         if self.args.appendcflags:
             self.args.CFLAGS += " " + self.args.appendcflags
         if self.args.appendcxxflags:
             self.args.CXXFLAGS += " " + self.args.appendcxxflags
+        if self.args.appendldflags:
+            self.args.LDFLAGS += " " + self.args.appendldflags
 
         if self.args.filelist:
             self._callfilelist()
