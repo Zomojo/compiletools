15d86a38cc0a6a919cfe1258347a8811a21e5c0a
DrGeoff <drgeoffathome@gmail.com>
2016-10-31 13:44:55 +1000

    WIP. Start of a test for apptools

---
+++ ct/test_apptools.py
@@ -0,0 +1,26 @@
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import unittest
+import ct.apptools
+
+class FakeNamespace(object):
+    def __init__(self):
+        self.n1 = 'v1_noquotes'
+        self.n2 = '"v2_doublequotes"'
+        self.n3 = "'v3_singlequotes'"
+    def __str__(self):
+        return str(self.__class__) + ": " + str(self.__dict__)
+
+class TestFuncs(unittest.TestCase):
+
+    def test_strip_quotes(self):
+        fns = FakeNamespace()
+        print(str(fns))
+        ct.apptools._strip_quotes(fns)
+        print(str(fns))
+        self.assertEqual(fns.n1, 'v1_noquotes')
+        self.assertTrue(False)
+
+if __name__ == '__main__':
+    unittest.main()
