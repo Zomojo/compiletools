daf5f70832f6eacecbda0ca900d261de4a4249bf
Geoff Ericksson <geoff.ericksson@zomojo.com>
2017-04-04 11:15:18 +1000

    Use psutil to figure out how many cpus are available for jobs

--- ct/jobs.py
+++ ct/jobs.py
@@ -3,15 +3,11 @@ from __future__ import unicode_literals
 
 import configargparse
 import ct.apptools
+import psutil
 
 def _cpus():
-    with open("/proc/cpuinfo") as ff:
-        proclines = [
-            line for line in ff.readlines() if line.startswith("processor")]
-    if 0 == len(proclines):
-        return 1
-    else:
-        return len(proclines)
+    thisprocess = psutil.Process()
+    return len(thisprocess.cpu_affinity())
 
 def add_arguments(cap):
     cap.add(
@@ -21,8 +17,8 @@ def add_arguments(cap):
         "--parallel",
         dest='parallel',
         type=int,
-        default=_cpus()-1,
-        help="Sets the number of CPUs to use in parallel for a build.  Defaults to 2 * all cpus.")
+        default=_cpus(),
+        help="Sets the number of CPUs to use in parallel for a build.")
         
 
 def main(argv=None):
@@ -30,7 +26,8 @@ def main(argv=None):
     ct.apptools.add_base_arguments(cap)
     add_arguments(cap)
     args = cap.parse_args(args=argv)
-    ct.apptools.verbose_print_args(args)
+    if args.verbose >= 2:
+        ct.apptools.verbose_print_args(args)
     print(args.parallel)
 
     return 0
