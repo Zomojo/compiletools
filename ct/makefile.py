Zomojo/compiletools

commit 7b5b726acad90e777480bcd0b675d58a63ccf8a1

All object files now live directly in the objectdir and not in a hierarchy below the objectdir. Some real world projects now build 25% faster.

ct/makefile.py
@@ -103,7 +103,7 @@ class LinkRuleCreator(object):
         recipe = ""
         if self.args.verbose >= 1:
             recipe += " ".join(["@echo ...", outputname, ";"])
-        recipe += " ".join(["mkdir -p", ct.wrappedos.dirname(outputname), ";", linker, "-o", outputname] +
+        recipe += " ".join([linker, "-o", outputname] +
                            list(object_names) +
                            list(all_magic_ldflags) +
                            [linkerflags])
@@ -481,6 +481,11 @@ class MakefileCreator:
         self.write(self.args.makefilename)
         return self.args.makefilename
 
+    def _create_object_directory(self):
+        return Rule(target='make_object_dir',
+                    prerequisites='',
+                    recipe=' '.join(['mkdir -p',self.args.objdir]))
+
     def _create_compile_rule_for_source(self, filename):
         """ For a given source file return the compile rule required for the Makefile """
         if self.args.verbose >=9:
@@ -492,7 +497,7 @@ class MakefileCreator:
                 filename)
 
         deplist = self.hunter.header_dependencies(filename)
-        prerequisites = [filename] + sorted([str(dep) for dep in deplist])
+        prerequisites = ['make_object_dir',filename] + sorted([str(dep) for dep in deplist])
 
         self.object_directories.add(self.namer.object_dir(filename))
         obj_name = self.namer.object_pathname(filename)
@@ -504,12 +509,12 @@ class MakefileCreator:
             recipe = " ".join(["@echo ...", filename, ";"])
         if ct.wrappedos.isc(filename):
             magic_c_flags = magicflags.get('CFLAGS', [])
-            recipe += " ".join(["mkdir -p", ct.wrappedos.dirname(obj_name),";", self.args.CC, self.args.CFLAGS]
+            recipe += " ".join([self.args.CC, self.args.CFLAGS]
                               + list(magic_c_flags)
                               + ["-c", "-o", obj_name, filename])
         else:
             magic_cxx_flags = magicflags.get('CXXFLAGS', [])
-            recipe += " ".join(["mkdir -p", ct.wrappedos.dirname(obj_name),";", self.args.CXX, self.args.CXXFLAGS]
+            recipe += " ".join([self.args.CXX, self.args.CXXFLAGS]
                               + list(magic_cxx_flags)
                               + ["-c", "-o", obj_name, filename])
 
@@ -551,12 +556,13 @@ class MakefileCreator:
 
     def _create_compile_rules_for_sources(self, sources):
         """ For all the given source files return the set of rules required
-            for the Makefile that will compil the source files into object files.
+            for the Makefile that will compile the source files into object files.
         """
 
         # The set of rules needed to turn the source file into an executable
         # (or library as appropriate)
         rules_for_source = ct.utils.OrderedSet()
+        rules_for_source.add(self._create_object_directory())
 
         # Output all the compile rules
         for source in sources:

ct/namer.py
@@ -44,21 +44,21 @@ class Namer(object):
 
     @memoize
     def object_dir(self, sourcefilename=None):
-        """ Put objects into a directory structure that starts with the
-            command line objdir but then replicates the project directory
-            structure.  This way we can separate object files that have
-            the same name but different paths.
+        """ This function allows for alternative behaviour to be explore.
+            Previously we tried replicating the source directory structure
+            to keep object files separated.  The mkdir involved slowed 
+            down the build process by about 25%.
         """
-        return self._outputdir(self.args.objdir, sourcefilename)
+        return self.args.objdir
 
     @memoize
     def object_name(self, sourcefilename):
         """ Return the name (not the path) of the object file
             for the given source.
         """
-        name = os.path.split(sourcefilename)[1]
+        directory,name = os.path.split(sourcefilename)
         basename = os.path.splitext(name)[0]
-        return "".join([basename, ".o"])
+        return "".join([directory.replace('/','@@'),'@@',basename, ".o"])
 
     @memoize
     def object_pathname(self, sourcefilename):
@@ -67,12 +67,10 @@ class Namer(object):
 
     @memoize
     def executable_dir(self, sourcefilename=None):
-        """ Put the binaries into a directory structure that starts with the
-            command line bindir but then replicates the project directory
-            structure.  This way we can separate executable files that have
-            the same name but different paths.
+        """ Similar to object_dir, this allows for alternative 
+            behaviour experimentation.
         """
-        return self._outputdir(self.args.bindir, sourcefilename)
+        return self.args.bindir
 
     @memoize
     def executable_name(self, sourcefilename):

ct/test_namer.py
@@ -35,10 +35,7 @@ class TestNamer(unittest.TestCase):
         namer = ct.namer.Namer(args, argv=argv, variant="gcc.debug")
         exename = namer.executable_pathname('/home/user/code/my.cpp')
         self.assertEqual(
-            exename,
-            os.path.join(
-                os.getcwd(),
-                'bin/gcc.debug/home/user/code/my'))
+            exename,'bin/gcc.debug/my')
     def tearDown(self):
         uth.reset()
 

commit f3dd71508cec9258f3072c5c58fe64be860e1848

7b5b726 introduced bug where the Makefile would always rebuild everything due to the dependency on the objectdir. Note that the timestamps on directory changes whenever a file is added, removed, or renamed.  The fix is to use order-only-prerequisites.

ct/makefile.py
@@ -18,7 +18,8 @@ import ct.namer
 
 class Rule:
 
-    """ A rule is a target, prerequisites and optionally a recipe
+    """ A rule is a target, prerequisites and optionally a recipe 
+        and optionally any order_only_prerequisites.
         https://www.gnu.org/software/make/manual/html_node/Rule-Introduction.html#Rule-Introduction
         Example: myrule = Rule( target='mytarget'
                               , prerequisites='file1.hpp file2.hpp'
@@ -27,9 +28,10 @@ class Rule:
         Note: it had to be a class rather than a dict so that we could hash it.
     """
 
-    def __init__(self, target, prerequisites, recipe=None, phony=False):
+    def __init__(self, target, prerequisites, order_only_prerequisites=None, recipe=None, phony=False):
         self.target = target
         self.prerequisites = prerequisites
+        self.order_only_prerequisites = order_only_prerequisites
         self.recipe = recipe
         self.phony = phony
 
@@ -50,7 +52,11 @@ class Rule:
         if self.phony:
             makefile.write(" ".join([".PHONY:", self.target, "\n"]))
 
-        makefile.write(self.target + ": " + self.prerequisites + "\n")
+        linetowrite = ''.join([self.target, ": ", self.prerequisites])
+        if self.order_only_prerequisites:
+            linetowrite += ''.join([" | ",self.order_only_prerequisites])
+
+        makefile.write( linetowrite + "\n" ) 
         try:
             makefile.write("\t" + self.recipe + "\n")
         except TypeError:
@@ -482,7 +488,7 @@ class MakefileCreator:
         return self.args.makefilename
 
     def _create_object_directory(self):
-        return Rule(target='make_object_dir',
+        return Rule(target=self.args.objdir,
                     prerequisites='',
                     recipe=' '.join(['mkdir -p',self.args.objdir]))
 
@@ -497,7 +503,7 @@ class MakefileCreator:
                 filename)
 
         deplist = self.hunter.header_dependencies(filename)
-        prerequisites = ['make_object_dir',filename] + sorted([str(dep) for dep in deplist])
+        prerequisites = sorted([str(dep) for dep in deplist])        
 
         self.object_directories.add(self.namer.object_dir(filename))
         obj_name = self.namer.object_pathname(filename)
@@ -521,8 +527,10 @@ class MakefileCreator:
         if self.args.verbose >= 3:
             print("Creating rule for ", obj_name)
 
+        # The order_only_prerequisite is to create the object directory
         return Rule(target=obj_name,
                     prerequisites=" ".join(prerequisites),
+                    order_only_prerequisites=self.args.objdir,
                     recipe=recipe)
 
     def _create_link_rules_for_sources(