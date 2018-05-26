9b75d0978715809968bd84ae093cd8a841ab496d
DrGeoff <drgeoffathome@gmail.com>
2018-05-26 14:00:06 +1000

    Make samples/library/build.sh more production ready

--- samples/library/build.sh
+++ samples/library/build.sh
@@ -4,9 +4,9 @@
 # If you would like to do a dynamic library (i.e., .so)
 # then use --dynamic rather than --static
 pushd mylib >/dev/null
-ct-cake --static get_numbers.cpp
+ct-cake --static get_numbers.cpp "$@"
 popd
 
 # Now build the main.cpp linking against the static library
-ct-cake --auto
+ct-cake --auto "$@"
 
