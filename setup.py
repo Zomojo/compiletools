3acb60e007aaf0b52f72129554e5122a8b475b5a
Geoff Ericksson <geoff.ericksson@zomojo.com>
2019-09-30 10:46:30 +1000

    Minor documentation changes

--- setup.py
+++ setup.py
@@ -30,7 +30,6 @@ setup(
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
     ],
     keywords='c++ make development',
